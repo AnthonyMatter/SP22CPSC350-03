80058b78696e0aa2f290639424b13b2223b70dee
AnthonyMatter <matter@chapman.edu>
2022-02-16 11:00:15 -0600

    Second part of Assignment2

---
+++ Assignment2/Parser.h
@@ -0,0 +1,47 @@
+#include <iostream>
+using namespace std;
+/*
+Function Name: Class is called Parser
+What it does: Contains the prototypes and declarations for everything I am going to use in this assignment.
+Parameters name and data types: No parameters but the data types are int, void, and string.
+Return value and type: No return value and type.
+*/
+class Parser{
+  public:
+    Parser();
+    int a,b,c,d,e,f,g,h,i,j,k,l,m;
+    int n,o,p,q,r,s,t,u,v,w,x,y,z;
+    int no_alpha;
+    int pair;
+    string line;
+    int Acount();
+    int Bcount();
+    int Ccount();
+    int Dcount();
+    int Ecount();
+    int Fcount();
+    int Gcount();
+    int Hcount();
+    int Icount();
+    int Jcount();
+    int Kcount();
+    int Lcount();
+    int Mcount();
+    int Ncount();
+    int Ocount();
+    int Pcount();
+    int Qcount();
+    int Rcount();
+    int Scount();
+    int Tcount();
+    int Ucount();
+    int Vcount();
+    int Wcount();
+    int Xcount();
+    int Ycount();
+    int Zcount();
+    int Non_Charactercount();
+    int NumberofAlpabets();
+    int Average();
+    int Pairs();
+};
